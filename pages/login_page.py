ff5704b99a53b8e5d0af85e6ca7631570030ecb0
Dima <zelenislo@yandex.ru>
2021-09-05 14:37:19 +0300

    add login_page and check login_url,login_regist,login_form

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,13 +8,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.open(), 'There is no login url'
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'There is no login form'
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert  self.is_element_present(*LoginPageLocators.LOGIN_REGISTER), 'There is no register_form'
